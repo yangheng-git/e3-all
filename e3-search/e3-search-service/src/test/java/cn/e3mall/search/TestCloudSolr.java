f41c66925e41774dd9e56d46c68c5ce9795f76e1
杨恒 <1660924019@qq.com>
2017-12-25 23:13:36 +0800

    solrCloud 测试

---
+++ e3-search/e3-search-service/src/test/java/cn/e3mall/search/TestCloudSolr.java
@@ -0,0 +1,77 @@
+package cn.e3mall.search;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+
+public class TestCloudSolr {
+
+	
+	/**
+	 * 创建一个集群的连接，应该使用CloudSolrServer创建
+	 * 			参数 zkhost : zookeeper的地址列表
+	 * 设置一个默认Collection属性
+	 * 创建一个文档对象
+	 * 向文档中添加域
+	 * 将文件写入索引库
+	 * 提交
+	 * @throws Exception 
+	 * @throws SolrServerException 
+	 */
+	@Test
+	public void testAddDocumentSolr() throws SolrServerException, Exception{
+		String zkhost="192.168.25.148:2181,192.168.25.148:2182,192.168.25.148:2183";
+		CloudSolrServer solrServer = new CloudSolrServer(zkhost);
+		
+		solrServer.setDefaultCollection("collection2");
+		
+		SolrInputDocument solrDocument = new SolrInputDocument();
+		solrDocument.setField("id", "solrCloud01");
+		solrDocument.setField("item_title","测试商品01");
+		solrDocument.setField("item_price", 123);
+		
+		solrServer.add(solrDocument);
+		solrServer.commit();
+		
+	}
+	
+	
+	
+	/**
+	 * 创建一个CloudSolrServer对象。 
+	 * 设置默认的Collection
+	 * 创建一个查询对象
+	 * 设置查询条件， 
+	 * 执行查询
+	 * 取查询结果
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testQueryCloudSolr() throws Exception{
+		String zkhost="192.168.25.148:2181,192.168.25.148:2182,192.168.25.148:2183";
+		CloudSolrServer solrServer = new CloudSolrServer(zkhost);
+		
+		solrServer.setDefaultCollection("collection2");
+		
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("*:*");
+		
+		QueryResponse queryResponse = solrServer.query(solrQuery);
+		SolrDocumentList results = queryResponse.getResults();
+		
+		System.out.println("总记录数：-------"+results.getNumFound());
+		for (SolrDocument solrDocument : results) {
+			System.out.println(solrDocument.get("id"));
+			System.out.println(solrDocument.get("item_title"));
+			System.out.println(solrDocument.get("item_image"));
+
+		}
+	}
+	
+}
