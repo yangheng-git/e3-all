fa65e3fec696ee205e0e8b549e2d1047afc8120f
杨恒 <1660924019@qq.com>
2017-12-31 23:38:55 +0800

    完成注册功能

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegitsterController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegitsterController.java
@@ -1,7 +1,15 @@
 package cn.e3mall.sso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.pojo.TbUser;
+import cn.e3mall.sso.service.RegitsterService;
 
 /**注册功能Controller
  * @author yangheng
@@ -9,9 +17,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class RegitsterController {
+    
+    @Autowired
+    private RegitsterService regitsterService;
 
+    /**展示注册页面
+     * @return
+     */
     @RequestMapping("/page/register")
     public String showRegister(){
         return "register";
+    }
+    
+    /**前端发来ajax请求。 判断数据的完整性。
+     * 
+     * @param param
+     * @param type
+     * @return e3Result 
+     * 如果填写的数据已村子，返回false
+     */
+    @RequestMapping("/user/check/{param}/{type}")
+    @ResponseBody
+    public E3Result checkDate(@PathVariable String param, @PathVariable Integer type ){
+        E3Result e3Result = regitsterService.checkdata(param, type);
+        return e3Result;
+    }
+    
+    
+    /*注册
+     * @param user
+     * @return
+     */
+    @RequestMapping(value="/user/regitster",method=RequestMethod.POST)
+    @ResponseBody
+    public E3Result regitster(TbUser user){
+        E3Result e3Result = regitsterService.regitster(user);
+        return e3Result;
     }
 }
---
+++ e3-sso/e3-sso-service/src/main/java/cn/e3mall/sso/serviice/impl/RegitsterServiceImpl.java
@@ -0,0 +1,99 @@
+package cn.e3mall.sso.serviice.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.mapper.TbUserMapper;
+import cn.e3mall.pojo.TbUser;
+import cn.e3mall.pojo.TbUserExample;
+import cn.e3mall.pojo.TbUserExample.Criteria;
+import cn.e3mall.sso.service.RegitsterService;
+
+/**
+ * @author yangheng 用户注册Service
+ */
+/**
+ * @author yangheng
+ *
+ */
+@Service
+public class RegitsterServiceImpl implements RegitsterService {
+
+    @Autowired
+    private TbUserMapper userMapper;
+
+    /**
+     * 检查用户注册 用户名，手机号，邮箱有没有被占用
+     * 
+     * @author yangheng
+     *
+     */
+    @Override
+    public E3Result checkdata(String param, Integer type) {
+        // 判断param的类型。 type=1 ：用户名 2：手机号 3：邮箱 。然后进行判断。
+        TbUserExample userExample = new TbUserExample();
+        Criteria criteria = userExample.createCriteria();
+        if (type == 1) {
+            criteria.andUsernameEqualTo(param);
+        } else if (type == 2) {
+            criteria.andPhoneEqualTo(param);
+        } else if (type == 3) {
+            criteria.andEmailEqualTo(param);
+        } else {
+            return E3Result.build(400, "错误的数据，请检查");
+        }
+        // 执行查询
+        List<TbUser> list = userMapper.selectByExample(userExample);
+        // 判断结果中是否包含数据。
+        if (list != null && list.size() > 0) {
+            // 如果有数据返回flase
+            return E3Result.ok(false);
+        }
+        // 如果没有数据返回true
+        return E3Result.ok(true);
+    }
+
+    /**
+     * (non-Javadoc) 注册Service
+     * 
+     * @see cn.e3mall.sso.service.RegitsterService#regitster(cn.e3mall.pojo.TbUser)
+     */
+    @Override
+    public E3Result regitster(TbUser user) {
+        // 注册前，判断数据的完整性。 和再次确认有没有被占用
+        if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())
+                || StringUtils.isBlank(user.getPhone())) {
+            // 有任意一项缺少，就不进行注册
+            return E3Result.build(400, "数据项缺少，请检查后重新注册");
+        }
+        // 判断数据项是否重复。 避免前端js验证失败的问题
+        E3Result checkdataResult= checkdata(user.getUsername(), 1);
+        if((boolean) checkdataResult.getData()){
+            return E3Result.build(400, "用户名被占用");
+        }
+        checkdataResult =  checkdata(user.getPhone(), 2);
+        if((boolean) checkdataResult.getData()){
+            return E3Result.build(400, "手机号被占用");
+        }
+        checkdataResult =  checkdata(user.getEmail(), 3);
+        if((boolean) checkdataResult.getData()){
+            return E3Result.build(400, "邮箱被占用");
+        }
+        //补全pojo属性
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        //对密码进行md5加密
+        String  md5pass = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(md5pass);
+        //把数据插入到数据库
+        userMapper.insert(user);
+        return E3Result.ok();
+    }
+
+}
